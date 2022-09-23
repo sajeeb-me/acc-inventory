b7d8f8ee2260f0206d16d52932006be8cac4fdfc
sdshuvo98 <hellosdshuvo@gamil.com>
2022-09-23 15:04:48 +0600

    acc inventory started

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+module.exports = app;
